0382eb155155423a03ee5bb32d8e10810108a98e
FDPW <Fabian10.D@wp.pl>
2024-08-27 22:33:55 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+function letter1(){
+    tytul = document.getElementById("tytul-tekst");
+    tekst = document.getElementById("tekst-tekst");
+
+    tytul.innerHTML = "Ja";
+    tekst.innerHTML = "Czy można prosić o lepszy prezent niż mnie? :) <br> Pewnie można, ale po co skoro lista moich zalet jest niesamowita, a oto i ona... <br><br> <b>Moje zalety:</b><br><ul id='zalety'><li>umiem przytulić</li><li>umiem pocałować</li><li>umiem zrobić tosty :D</li><li>perfekcyjnie rozkrajam bułeczki</li><li>wcale</li><li>nie</li><li>przedłużam</li><li>sztucznie</li><li>tej</li><li>listy</li><li>mam skarbonkę kinder niespodzianki i zdolne rączki :o</li></ul><br><br>Oferta specjalna na takiego chłopaka jest dostępna tylko dla ciebie, także korzystaj i dbaj :)";
+}
+
+function letter2(){
+    tytul = document.getElementById("tytul-tekst");
+    tekst = document.getElementById("tekst-tekst");
+
+    tytul.innerHTML = "Absolutnie";
+    tekst.innerHTML = "Razem ze wspomnianym wcześniej <b>przystojniakiem</b>, otrzymujesz absolutną gwarancję cudownie spędzonych razem <b>dni</b> i <b>nocy</b> ;)<br><br>W pakiecie dodatków otrzymujesz także <b><i>kochającego cię całym serduszkiem chłopaka</b></i>, który posiada funkcje <b>grzejniczka i podusi/materacu</b>.";
+}
+
+function letter3(){
+    tytul = document.getElementById("tytul-tekst");
+    tekst = document.getElementById("tekst-tekst");
+
+    tytul.innerHTML = "Cię";
+    tekst.innerHTML = "Wiem, że nie uważasz się za idealną, ale <b>ja cię za taką uważam (<mark>nie przyjmuję odmowy</mark>)</b>.<br><br>Jest tylko <b>jedna</b> kobieta o której marzyłem i przy której zawsze chciałbym być, nie zgadniesz kto to taki.<br><b><i>(Podpowiedź: Ma prześliczne długie blond włosy, piękne oczy, a jej imię zaczyna się na 'W', a kończy na 'iktoria')</b></i>";
+}
+
+function letter4(){
+    tytul = document.getElementById("tytul-tekst");
+    tekst = document.getElementById("tekst-tekst");
+
+    tytul.innerHTML = "Kocham";
+    tekst.innerHTML = "<b>Kocham wszystko</b> w tobie, począwszy od wyglądu, a na charakterze kończąc. <b><i>Jesteś cudowna <3</b></i><br><br>Chcesz wiedzieć co konkretnie w tobie kocham? Proszę bardzo oto <b><i>BARDZO BARDZO BARDZO</b></i> skrócona lista:<br><ul id='zalety'><li>włosy</li><li>charakter</li><li>poczucie humoru</li><li>oczy</li><li>ciałko :D</li><li>nóżki</li><li>rączki</li><li>usta</li><li>twarzyczkę <i>(nawet bez makijażu, skarbie <3)</i></li><li>upartość</li><li>siłę do wytrzymywania ze mną</li></ul><br><b>i WIELE WIELE WIELE WIELE WIELE więcej <3</b>";
+}
+
+function letter5(){
+    tytul = document.getElementById("tytul-tekst");
+    tekst = document.getElementById("tekst-tekst");
+
+    tytul.innerHTML = "<3";
+    tekst.innerHTML = "Czy te 'listy' wyglądają na totalnie losowe? Być może, ale to tylko dlatego, że są pisane <b>prosto z serduszka</b>, a jak wiesz, nie istnieją słowa, które są w stanie opisać to jak <b>bardzo cię kocham <3</b><br><br>Pamiętaj, jeśli kiedykolwiek coś będzie nie tak, będziesz potrzebować pomocy, wygadać się, wyszaleć się, albo po prostu się przytulić to <b>zawsze jestem tuż obok ciebie <3</b><br><br><br>Skoro już wiesz jak <b>bardzo, bardzo, bardzo, bardzo, bardzo, bardzo cię kocham</b> to pamiętaj też, że zawsze możesz na mnie liczyć, zawsze będę przy tobie i zawsze zrobię dla ciebie wszystko, abyś była szczęśliwa jak przez te ostatnie 6 miesięcy ze mną :D<br><b><br>Teraz przeczytaj tytuły wszystkich kopert po kolei od góry do dołu <3</b>";
+}
